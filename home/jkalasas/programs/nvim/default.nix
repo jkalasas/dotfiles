a73e2c573a826160d3e1c8094e9ebcb9458af21e
jkalasas <alasasjohnkyle@gmail.com>
2023-12-28 17:11:13 +0800

    lsp config

--- home/jkalasas/programs/nvim/default.nix
+++ home/jkalasas/programs/nvim/default.nix
@@ -30,13 +30,45 @@
 		 type = "lua";
 		 config = "require(\"Comment\").setup()";
 	   }
+
+       cmp_luasnip
+       cmp-nvim-lsp
+       luasnip
+       {
+         plugin = nvim-cmp;
+         type = "lua";
+         config = getFile ./config/plugins/cmp.lua;
+       }
+
+       {
+         plugin = nvim-lspconfig;
+         type = "lua";
+         config = getFile ./config/plugins/lsp.lua;
+       }
+
+       neodev-nvim
+
 	   {
 	     plugin = telescope-nvim;
 		 type = "lua";
-		 config = getFile ./config/plugins/telescope.lua};
+		 config = getFile ./config/plugins/telescope.lua;
 	   }
 	   telescope-fzf-native-nvim
-	   vim-nix
+
+       {
+         plugin = (nvim-treesitter.withPlugins (p: [
+           p.tree-sitter-nix
+           p.tree-sitter-vim
+           p.tree-sitter-bash
+           p.tree-sitter-lua
+           p.tree-sitter-python
+           p.tree-sitter-json
+         ]);
+         type = "lua";
+         config = getFile ./config/plugins/treesitter.lua;
+       }
+
+       vim-nix
 	 ];
 
      extraLuaConfig = getFile ./config/options.lua;
