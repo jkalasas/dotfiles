5cb0b2302df7bab73cca2505cea1ed4514fd1f64
jkalasas <alasasjohnkyle@gmail.com>
2023-12-28 16:44:33 +0800

    vim telescope

--- home/jkalasas/programs/nvim/default.nix
+++ home/jkalasas/programs/nvim/default.nix
@@ -15,8 +15,25 @@
        wl-clipboard
      ];
 
+	 plugins = with pkgs.vimPlugins; [
+	   {
+	     plugin = comment-nvim;
+		 type = "lua";
+		 config = "require(\"Comment\").setup()";
+	   }
+	   {
+	     plugin = telescope-nvim;
+		 type = "lua";
+		 config = "${builtins.readFile ./config/plugins/telescope.lua}";
+	   }
+	   telescope-fzf-native-nvim
+	   vim-nix
+	 ];
+
      extraLuaConfig = ''
        ${builtins.readFile ./config/options.lua}
      '';
   };
 }
+
+# thanks to vimjoyer <3
