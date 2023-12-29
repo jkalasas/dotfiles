66cd03e3407f258d16142541cac84b81df5a7c80
jkalasas <alasasjohnkyle@gmail.com>
2023-12-29 13:49:14 +0800

    fixed wayland screenshots

--- home/jkalasas/scripts/screenshot.nix
+++ home/jkalasas/scripts/screenshot.nix
@@ -9,28 +9,27 @@ let
     geometry=`xrandr | head -n1 | cut -d',' -f2 | tr -d '[:blank:],current'`
     dir="`xdg-user-dir PICTURES`/Screenshots"
     file="Screenshot_''${time}_''${geometry}.png"
+    full_path="''${dir}/$file"
 
     # Buttons
     layout=`cat $rasi_config | grep BUTTON | cut -d'=' -f2 | tr -d '[:blank:],*/'`
     if [[ "$layout" == "TRUE" ]]; then
-    	screen=""
-    	area=""
-    	window="缾"
-    	infive="靖"
-    	inten="福"
+    	screen="󰍹 "
+    	area="󰹑 "
+    	infive="󰔝 "
+    	inten="󰔜 "
     else
-    	screen=" Capture Desktop"
-    	area=" Capture Area"
-    	window="缾 Capture Window"
-    	infive="靖 Take in 3s"
-    	inten="福 Take in 10s"
+    	screen="󰍹  Capture Desktop"
+    	area="󰹑  Capture Area"
+    	infive="󰔝  Take in 3s"
+    	inten="󰔜  Take in 10s"
     fi
 
     # Notify and view screenshot
     notify_view () {
     	dunstify -u low --replace=699 "Copied to clipboard."
-    	eog ''${dir}/"$file"
-    	if [[ -e "$dir/$file" ]]; then
+    	eog $full_path
+    	if [[ -e $full_path ]]; then
     		dunstify -u low --replace=699 "Screenshot Saved."
     	else
     		dunstify -u low --replace=699 "Screenshot Deleted."
@@ -47,30 +46,26 @@ let
 
     # take shots
     shotnow () {
-    	cd ''${dir} && sleep 0.5 && maim -u -f png | tee "$file" | xclip -selection clipboard -t image/png
-    	notify_view
+      sleep 1
+      grim $full_path
+      wl-copy < $full_path
+      notify_view
     }
 
     shot5 () {
-    	countdown '3'
-    	sleep 1 && cd ''${dir} && maim -u -f png | tee "$file" | xclip -selection clipboard -t image/png
-    	notify_view
+      countdown '3'
+      shotnow
     }
 
     shot10 () {
-    	countdown '10'
-    	sleep 1 && cd ''${dir} && maim -u -f png | tee "$file" | xclip -selection clipboard -t image/png
-    	notify_view
-    }
-
-    shotwin () {
-    	cd ''${dir} && maim -u -f png -i `xdotool getactivewindow` | tee "$file" | xclip -selection clipboard -t image/png
-    	notify_view
+      countdown '10'
+      shownow
     }
 
     shotarea () {
-    	cd ''${dir} && maim -u -f png -s -b 2 -c 0.35,0.55,0.85,0.25 -l | tee "$file" | xclip -selection clipboard -t image/png
-    	notify_view
+      grim -g "$(slurp)" $full_path
+      wl-copy < $full_path
+      notify_view
     }
 
     if [[ ! -d "$dir" ]]; then
@@ -78,7 +73,7 @@ let
     fi
 
     # Variable passed to rofi
-    options="$screen\n$area\n$window\n$infive\n$inten"
+    options="$screen\n$area\n$infive\n$inten"
 
     chosen="$(echo -e "$options" | $rofi_command -p 'Take A Shot' -dmenu -selected-row 0)"
     case $chosen in
@@ -88,9 +83,6 @@ let
         $area)
     		shotarea
             ;;
-        $window)
-    		shotwin
-    		;;
         $infive)
     		shot5
     		;;
@@ -100,13 +92,14 @@ let
     esac
   '';
 in
-  {
-    home.packages = with pkgs; [
-      # nix
-      maim
-      xclip
+{
+  home.packages = with pkgs; [
+    # nix
+    grim
+    slurp
+    wl-clipboard
 
-      # script
-      screenshot
-    ];
+    # script
+    screenshot
+  ];
 }
