67fcb10ea447787160c87611fe839c192adc21b2
jkalasas <alasasjohnkyle@gmail.com>
2023-12-30 14:22:29 +0800

    zathura catppuccin macchiato

--- home/jkalasas/programs/zathura/default.nix
+++ home/jkalasas/programs/zathura/default.nix
@@ -1,6 +1,12 @@
 { config, pkgs, ... }:
 {
+  home.file."${config.xdg.configHome}/zathura/catppuccin-macchiato".source = ./catppuccin-macchiato;
+
   programs.zathura = {
     enable = true;
+
+    extraConfig = ''
+      include catppuccin-macchiato
+    '';
   };
 }
