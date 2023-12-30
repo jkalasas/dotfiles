ef751ec005fb820125fcdd96cccfd1b38c88133c
jkalasas <alasasjohnkyle@gmail.com>
2023-12-30 13:12:34 +0800

    nvim markdown support

--- home/jkalasas/programs/nvim/config/options.lua
+++ home/jkalasas/programs/nvim/config/options.lua
@@ -1,12 +1,12 @@
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 
 vim.o.number = true
 -- vim.o.relativenumber = true
 
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
@@ -15,51 +15,67 @@ vim.o.updatetime = 300
 
 vim.o.termguicolors = true
 
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
+
+-- markdown-preview
+vim.api.nvim_exec(
+	[[
+	function OpenMarkdownPreview (url)
+    	execute "silent ! firefox --new-window " . a:url
+	endfunction
+	let g:mkdp_browserfunc = 'OpenMarkdownPreview'
+]],
+	false
+)
 
 local M = {
 	n = {
 		-- comment
-		['<leader>/'] = function()
+		["<leader>/"] = function()
 			require("Comment.api").toggle.linewise.current()
 		end,
 
 		-- save
-		['<C-s>'] = '<cmd> w <CR>',
+		["<C-s>"] = "<cmd> w <CR>",
 
 		-- window navigation
-		['<C-h>'] = '<C-w>h',
-		['<C-l>'] = '<C-w>l',
-		['<C-j>'] = '<C-w>j',
-		['<C-k>'] = '<C-w>k',
+		["<C-h>"] = "<C-w>h",
+		["<C-l>"] = "<C-w>l",
+		["<C-j>"] = "<C-w>j",
+		["<C-k>"] = "<C-w>k",
 
 		-- buffers
-		['<leader>t'] = '<cmd> enew <CR>',
-		['<leader>w'] = '<cmd> bd <CR>',
-		['<leader>x'] = '<cmd> bd <CR>',
-		['<tab>'] = '<cmd> bnext <CR>',
-		['<S-tab>'] = '<cmd> bprevious <CR>',
+		["<leader>t"] = "<cmd> enew <CR>",
+		["<leader>w"] = "<cmd> bd <CR>",
+		["<leader>x"] = "<cmd> bd <CR>",
+		["<tab>"] = "<cmd> bnext <CR>",
+		["<S-tab>"] = "<cmd> bprevious <CR>",
 
 		-- formatter
-		['<leader>fm'] = function()
-			vim.lsp.buf.format { async = true };
-        end,
+		["<leader>fm"] = function()
+			vim.lsp.buf.format({ async = true })
+		end,
+
+		-- markdown
+		["<leader>mp"] = "<cmd> MarkdownPreview <CR>",
+		["<leader>ms"] = "<cmd> MarkdownPreviewStop <CR>",
+		["<leader>mt"] = "<cmd> MarkdownPreviewToggle <CR>",
 
 		-- nvim-tree
-		['<C-n>'] = '<cmd> NvimTreeToggle <CR>',
-		['<leader>e'] = '<cmd> NvimTreeFocus <CR>',
+		["<C-n>"] = "<cmd> NvimTreeToggle <CR>",
+		["<leader>e"] = "<cmd> NvimTreeFocus <CR>",
 
 		-- telescope
-		['<leader>ff'] = '<cmd> Telescope find_files <CR>',
-		['<leader>fa'] = '<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>',
-		['<leader>fw'] = '<cmd> Telescope live_grep <CR>',
-		['<leader>fb'] = '<cmd> Telescope buffers <CR>',
+		["<leader>ff"] = "<cmd> Telescope find_files <CR>",
+		["<leader>fa"] = "<cmd> Telescope find_files follow=true no_ignore=true hidden=true <CR>",
+		["<leader>fw"] = "<cmd> Telescope live_grep <CR>",
+		["<leader>fb"] = "<cmd> Telescope buffers <CR>",
 	},
 
 	v = {
-		['<leader>/'] = "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
+		["<leader>/"] = "<ESC><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>",
 	},
 }
 
--- home/jkalasas/programs/nvim/default.nix
+++ home/jkalasas/programs/nvim/default.nix
@@ -1,120 +1,125 @@
 { config, lib, pkgs, ... }:
-{ programs.neovim = 
-  let
-    getFile = file: "${builtins.readFile file}";
-  in
-  {
-    enable = true;
+{
+  programs.neovim =
+    let
+      getFile = file: "${builtins.readFile file}";
+    in
+    {
+      enable = true;
 
-     viAlias = true;
-     vimAlias = true;
-     vimdiffAlias = true;
+      viAlias = true;
+      vimAlias = true;
+      vimdiffAlias = true;
 
-     extraPackages = with pkgs; [
-       # language servers
-       emmet-ls
-       nodePackages.pyright
-       typescript
-       nodePackages.svelte-language-server
-       nodePackages.typescript-language-server
-       lua-language-server
-       luajitPackages.lua-lsp
-       tailwindcss-language-server
+      extraPackages = with pkgs; [
+        # language servers
+        emmet-ls
+        nodePackages.pyright
+        typescript
+        nodePackages.svelte-language-server
+        nodePackages.typescript-language-server
+        lua-language-server
+        luajitPackages.lua-lsp
+        tailwindcss-language-server
 
-       # formatters
-       black
-       prettierd
-       stylua
+        # formatters
+        black
+        prettierd
+        stylua
 
-       # linters
-       eslint_d
+        # linters
+        eslint_d
 
-       rnix-lsp
+        rnix-lsp
 
-       fd
-       ripgrep
-       xclip
-       wl-clipboard
-     ];
+        fd
+        ripgrep
+        xclip
+        wl-clipboard
+      ];
 
-     plugins = with pkgs.vimPlugins; [
-       {
-         plugin = bufferline-nvim;
-         type = "lua";
-         config = "require(\"bufferline\").setup{}";
-       }
-       {
-         plugin = catppuccin-nvim;
-         type = "lua";
-         config = getFile ./config/plugins/catppuccin.lua;
-       }
-	   {
-	     plugin = comment-nvim;
-		 type = "lua";
-		 config = "require(\"Comment\").setup{}";
-	   }
+      plugins = with pkgs.vimPlugins; [
+        {
+          plugin = bufferline-nvim;
+          type = "lua";
+          config = "require(\"bufferline\").setup{}";
+        }
+        {
+          plugin = catppuccin-nvim;
+          type = "lua";
+          config = getFile ./config/plugins/catppuccin.lua;
+        }
+        {
+          plugin = comment-nvim;
+          type = "lua";
+          config = "require(\"Comment\").setup{}";
+        }
 
-       cmp_luasnip
-       cmp-nvim-lsp
-       luasnip
-       {
-         plugin = nvim-cmp;
-         type = "lua";
-         config = getFile ./config/plugins/cmp.lua;
-       }
+        cmp_luasnip
+        cmp-nvim-lsp
+        luasnip
+        {
+          plugin = nvim-cmp;
+          type = "lua";
+          config = getFile ./config/plugins/cmp.lua;
+        }
 
-       {
-         plugin = nvim-lspconfig;
-         type = "lua";
-         config = getFile ./config/plugins/lsp.lua;
-       }
+        {
+          plugin = nvim-lspconfig;
+          type = "lua";
+          config = getFile ./config/plugins/lsp.lua;
+        }
 
-       {
-         plugin = lualine-nvim;
-         type = "lua";
-         config = "require('lualine').setup()";
-       }
+        {
+          plugin = lualine-nvim;
+          type = "lua";
+          config = "require('lualine').setup()";
+        }
 
-       neodev-nvim
+        neodev-nvim
 
-       {
-         plugin = null-ls-nvim;
-         type = "lua";
-         config = getFile ./config/plugins/null-ls.lua;
-       }
+        {
+          plugin = null-ls-nvim;
+          type = "lua";
+          config = getFile ./config/plugins/null-ls.lua;
+        }
 
-       {
-         plugin = nvim-tree-lua;
-         type = "lua";
-         config = getFile ./config/plugins/nvim-tree.lua;
-       }
+        {
+          plugin = nvim-tree-lua;
+          type = "lua";
+          config = getFile ./config/plugins/nvim-tree.lua;
+        }
 
-	   {
-	     plugin = telescope-nvim;
-		 type = "lua";
-		 config = getFile ./config/plugins/telescope.lua;
-	   }
-	   telescope-fzf-native-nvim
+        markdown-preview-nvim
 
-       {
-         plugin = (nvim-treesitter.withPlugins (p: [
-           p.tree-sitter-nix
-           p.tree-sitter-vim
-           p.tree-sitter-bash
-           p.tree-sitter-lua
-           p.tree-sitter-python
-           p.tree-sitter-json
-         ]));
-         type = "lua";
-         config = getFile ./config/plugins/treesitter.lua;
-       }
+        {
+          plugin = telescope-nvim;
+          type = "lua";
+          config = getFile ./config/plugins/telescope.lua;
+        }
+        telescope-fzf-native-nvim
 
-       vim-nix
-       vim-tmux-navigator
-	 ];
+        {
+          plugin = (nvim-treesitter.withPlugins (p: [
+            p.tree-sitter-nix
+            p.tree-sitter-vim
+            p.tree-sitter-bash
+            p.tree-sitter-lua
+            p.tree-sitter-markdown
+            p.tree-sitter-markdown-inline
+            p.tree-sitter-python
+            p.tree-sitter-json
+          ]));
+          type = "lua";
+          config = getFile ./config/plugins/treesitter.lua;
+        }
 
-     extraLuaConfig = getFile ./config/options.lua;
-  };
+        vim-nix
+        vim-tmux-navigator
+      ];
+
+      extraLuaConfig = getFile ./config/options.lua;
+    };
 }
 
 # thanks to vimjoyer <3
